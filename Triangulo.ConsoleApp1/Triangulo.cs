88fcd0dfb3b97d47e023c985e33e0ea0b9d8e7e4
Diego <Diego@DESKTOP-VKT3JJP>
2023-04-10 21:20:36 -0300

    Add project files.

---
+++ Triangulo.ConsoleApp1/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo.ConsoleApp1
+{
+    internal class Triangulo
+    {
+        public double lado1 { get; set; }
+        public double lado2 { get; set; }
+        public double lado3 { get; set; }
+
+        public void CalcularTriangulo()
+        {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                if (lado1 == lado2 && lado1 == lado3)
+                {
+                    Console.WriteLine("\nÉ um triângulo equilátero");
+                }
+                else if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3)
+                {
+                    Console.WriteLine("\nÉ um triângulo escaleno");
+                }
+                else
+                {
+                    Console.WriteLine("\nÉ um triângulo isósceles");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNão é um triângulo");
+            }
+        }
+    }
+}
